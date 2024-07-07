b08377ae38153b0fb9985ae19da31b33776b2d88
Lua Deolvr <69927562+LuaGeo@users.noreply.github.com>
2024-07-07 23:03:36 +0200

    procfile and port changed

--- app.py
+++ app.py
@@ -29,5 +29,6 @@ setup_favorite_movies_routes(app)
 setup_recommendations_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
