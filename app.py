c9459ba3513ca8822059274698fa31160a533e18
Lua Deolvr <69927562+LuaGeo@users.noreply.github.com>
2024-07-07 22:57:14 +0200

    working local

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ setup_favorite_movies_routes(app)
 setup_recommendations_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
 
